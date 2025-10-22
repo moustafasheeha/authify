84d832338781e95ec1c0b942133728923cb3f7a1
MustafaSheha <mustafa.mahmoud.sheha@gmail.com>
2025-10-22 12:14:19 +0300

    Add AuthController for managing authentication state and user actions

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,62 @@
+import 'package:get/get.dart';
+import 'package:authify/core/repo/i_auth_repo.dart';
+import 'package:authify/core/errors/base_failure.dart';
+
+class AuthController extends GetxController {
+  final IAuthRepo _repo;
+
+  AuthController(this._repo);
+
+  final isLoading = false.obs;
+  final isLoggedIn = false.obs;
+  final errorMessage = ''.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _repo.authStateChanges.listen((loggedIn) {
+      isLoggedIn.value = loggedIn;
+      if (loggedIn) errorMessage.value = '';
+    }, onError: (err) {
+      errorMessage.value = 'Authentication status error.';
+    });
+  }
+
+  Future<void> signUp({required String email, required String password}) async {
+    isLoading.value = true;
+    errorMessage.value = '';
+    try {
+      await _repo.signUp(email: email, password: password);
+    } on BaseFailure catch (f) {
+      errorMessage.value = f.message;
+    } catch (e) {
+      errorMessage.value = 'Unexpected error occurred.';
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> signIn({required String email, required String password}) async {
+    isLoading.value = true;
+    errorMessage.value = '';
+    try {
+      await _repo.signIn(email: email, password: password);
+    } on BaseFailure catch (f) {
+      errorMessage.value = f.message;
+    } catch (e) {
+      errorMessage.value = 'Unexpected error occurred.';
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _repo.signOut();
+    } on BaseFailure catch (f) {
+      errorMessage.value = f.message;
+    } catch (e) {
+      errorMessage.value = 'Unexpected error while signing out.';
+    }
+  }
+}
